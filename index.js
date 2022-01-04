7bf757fbc49b4305edfbabea87a5ae35bba7ebd5
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-01-04 11:42:32 -0300

    add authors/id route with get action

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ const books = require('./models/books');
 
 app.listen(port, () => { console.log(`Ouvindo a porta ${port}`) })
 
+// Retorna todos os autores
 app.get('/authors', async (_req, res) => {
   const authors = await author.getAll()
   return res.status(200).json(authors)
 });
 
+// Retorna o autor pelo id
+app.get('authors/:id', async (req, res) => {
+  const authorId = req.params.id;
+  const author = await author.getAuthorById(authorId);
+  if (!author) return res.status(404).json({ message: 'Not Found' });
+  return res.status(200).json(author);
+})
+
+// Retorna todos os livros
 app.get('/books', async(_req, res) => {
   const allBooks = await books.getAll();
   return res.status(200).json(allBooks);
 });
 
+// Retorna os livros por authorId
 app.get('/books/:id', async (req, res) => {
-  const booksById = await books.getByAuthorId(req.params.id);
+  const booksById = await books.getBooksByAuthorId(req.params.id);
   if(!booksById) return res.status(404).json({ message: 'Not found' });
   return res.status(200).json(booksById);
 })
